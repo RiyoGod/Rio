b5eca8d9867a6b7707e84109e5bc34e6759d494d
RiyoGod <godsxrio@gmail.com>
2025-02-06 03:12:18 +0530

    Update start.py

--- RioAdBot/plugins/start.py
+++ RioAdBot/plugins/start.py
@@ -4,7 +4,28 @@ import logging
 import os
 from dotenv import load_dotenv
 from telegram.ext import Application, CommandHandler
-from RioAdBot.plugins.welcome import start
+from telegram import Update
+from telegram.ext import CallbackContext
+
+async def start_command(update: Update, context: CallbackContext):
+    """Handles the /start command."""
+    user = update.effective_user
+    first_name = user.first_name
+
+    welcome_message = (
+        f"**Hello, {first_name}!**\n"
+        "Welcome to **@BoostAdvertBot**, your go-to advertising solution.\n\n"
+        "✨ *What can I do for you?*\n"
+        "- **Exclusive advertising services**\n"
+        "- **Premium & affordable plans**\n"
+        "- **Secure crypto payments**\n\n"
+        "__Get started now!__\n"
+        "Use **/purchase** to explore our plans and begin.\n\n"
+        "Need help? Contact **@Boostadvert** for support."
+    )
+
+    await update.message.reply_text(welcome_message, parse_mode="Markdown")
+
 
 # Load environment variables
 load_dotenv()
