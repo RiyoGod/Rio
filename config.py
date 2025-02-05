428d13331b763ffe4b8df272bfc4d8d4387ddf5a
RiyoGod <godsxrio@gmail.com>
2025-02-05 23:46:30 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-CRYPTOBOT_SECRET = os.getenv("CRYPTOBOT_SECRET" , "335393:AAZMPAfpnvAFGLApXm4BTKVbxNAuTVxbd9t")
+CRYPTOBOT_SECRET = os.getenv("CRYPTOBOT_SECRET" , "335607:AA3yJu1fkPWWbczmD6hw8uesXCiAwzIJWm1")
 TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN" , "7717505592:AAFprS-Sc-W34Sm2pfJ8srkPw1e91qbnoxY")
 BOT_TOKEN = os.getenv("BOT_TOKEN" , "7717505592:AAFprS-Sc-W34Sm2pfJ8srkPw1e91qbnoxY")
 
