c7ee45e09df6fcf6f1a472ecae8398bfc567e0d4
RiyoGod <godsxrio@gmail.com>
2025-02-05 23:11:57 +0530

    Update bot.py

--- RioAdBot/bot.py
+++ RioAdBot/bot.py
@@ -1,17 +1,18 @@
 from telegram import Update
-from telegram.ext import Updater, CommandHandler, CallbackContext
-from .plugins.start import start
+from telegram.ext import Application, CommandHandler, CallbackContext
+from RioAdBot.plugins.start import start
 from RioAdBot.plugins.purchase import purchase
 
+TOKEN = "7717505592:AAFprS-Sc-W34Sm2pfJ8srkPw1e91qbnoxY"
+
 def main():
-    updater = Updater("7717505592:AAFprS-Sc-W34Sm2pfJ8srkPw1e91qbnoxY", use_context=True)
-    dp = updater.dispatcher
+    app = Application.builder().token(TOKEN).build()
 
-    dp.add_handler(CommandHandler("start", start))
-    dp.add_handler(CommandHandler("purchase", purchase))
+    app.add_handler(CommandHandler("start", start))
+    app.add_handler(CommandHandler("purchase", purchase))
 
-    updater.start_polling()
-    updater.idle()
+    print("Bot is running...")
+    app.run_polling()
 
 if __name__ == "__main__":
     main()
