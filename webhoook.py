0841aea0ab93ea72821d5e8a6d3c76684f0014d3
DeadliestOne <uncountableaura@gmail.com>
2025-02-05 20:00:09 +0530

    Update webhoook.py

--- webhoook.py
+++ webhoook.py
@@ -7,8 +7,8 @@ from telegram import Bot
 load_dotenv()
 
 # Get API tokens from environment variables
-CRYPTOBOT_SECRET = os.getenv("CRYPTOBOT_SECRET")
-TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
+CRYPTOBOT_SECRET = "335393:AAZMPAfpnvAFGLApXm4BTKVbxNAuTVxbd9t"
+TELEGRAM_BOT_TOKEN = "7717505592:AAFprS-Sc-W34Sm2pfJ8srkPw1e91qbnoxY"
 
 if not CRYPTOBOT_SECRET or not TELEGRAM_BOT_TOKEN:
     raise ValueError("Missing API tokens! Set CRYPTOBOT_SECRET and TELEGRAM_BOT_TOKEN in .env")
