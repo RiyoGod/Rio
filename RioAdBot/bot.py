10d7ba156c0e24b80a57fce8b831bde306f9a047
DeadliestOne <uncountableaura@gmail.com>
2025-02-05 20:26:33 +0530

    Update bot.py

--- RioAdBot/bot.py
+++ RioAdBot/bot.py
@@ -1,15 +1,14 @@
-from telegram.ext import Updater, CommandHandler, CallbackQueryHandler
-from config import TELEGRAM_BOT_TOKEN
-from plugins.start import start
-from plugins.purchase import purchase
+from telegram import Update
+from telegram.ext import Updater, CommandHandler, CallbackContext
+from RioAdBot.plugins.start import start
+from RioAdBot.plugins.purchase import purchase
 
 def main():
-    updater = Updater(TELEGRAM_BOT_TOKEN, use_context=True)
+    updater = Updater("7717505592:AAFprS-Sc-W34Sm2pfJ8srkPw1e91qbnoxY", use_context=True)
     dp = updater.dispatcher
 
     dp.add_handler(CommandHandler("start", start))
     dp.add_handler(CommandHandler("purchase", purchase))
-    dp.add_handler(CallbackQueryHandler(purchase))  # Handles button clicks
 
     updater.start_polling()
     updater.idle()
