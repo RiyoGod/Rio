61962750546396b329a22f93190880b3bf24133a
RiyoGod <godsxrio@gmail.com>
2025-02-05 23:14:59 +0530

    Update bot.py

--- RioAdBot/bot.py
+++ RioAdBot/bot.py
@@ -1,7 +1,6 @@
-from telegram import Update
-from telegram.ext import Application, CommandHandler, CallbackContext
+from telegram.ext import Application, CommandHandler, CallbackQueryHandler
 from RioAdBot.plugins.start import start
-from RioAdBot.plugins.purchase import purchase
+from RioAdBot.plugins.purchase import purchase, button_handler
 
 TOKEN = "7717505592:AAFprS-Sc-W34Sm2pfJ8srkPw1e91qbnoxY"
 
@@ -10,6 +9,7 @@ def main():
 
     app.add_handler(CommandHandler("start", start))
     app.add_handler(CommandHandler("purchase", purchase))
+    app.add_handler(CallbackQueryHandler(button_handler))
 
     print("Bot is running...")
     app.run_polling()
