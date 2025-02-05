a39dc309a0bdc732f9400af3a651495953381e03
RiyoGod <godsxrio@gmail.com>
2025-02-06 03:13:41 +0530

    Update start.py

--- RioAdBot/plugins/start.py
+++ RioAdBot/plugins/start.py
@@ -1,9 +1,3 @@
-
-
-import logging
-import os
-from dotenv import load_dotenv
-from telegram.ext import Application, CommandHandler
 from telegram import Update
 from telegram.ext import CallbackContext
 
@@ -25,33 +19,3 @@ async def start_command(update: Update, context: CallbackContext):
     )
 
     await update.message.reply_text(welcome_message, parse_mode="Markdown")
-
-
-# Load environment variables
-load_dotenv()
-TOKEN = os.getenv('BOT_TOKEN')
-
-if TOKEN is None:
-    raise ValueError("Bot token is not set in the .env file")
-
-logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-                    level=logging.INFO)
-logger = logging.getLogger(__name__)
-
-async def purchase_command(update, context):
-    """Handles the /purchase command."""
-    from RioAdBot.plugins.purchase import show_plans  # Lazy import
-    await show_plans(update, context)
-
-
-
-def main():
-    app = Application.builder().token(TOKEN).build()
-    app.add_handler(CommandHandler("start", start))
-    app.add_handler(CommandHandler("purchase", purchase_command))
-
-    logger.info("Bot is running...")
-    app.run_polling()
-
-if __name__ == '__main__':
-    main()
