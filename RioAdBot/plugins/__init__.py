d51a2af9182102754d856245275b0d323fa5061c
RiyoGod <godsxrio@gmail.com>
2025-02-06 01:18:58 +0530

    Update __init__.py

--- RioAdBot/plugins/__init__.py
+++ RioAdBot/plugins/__init__.py
@@ -1,10 +1,4 @@
 from .start import start
 from .purchase import purchase
 
-def get_start():
-    return start
-
-def get_purchase():
-    return purchase
-
-__all__ = ["get_start", "get_purchase"]
+__all__ = ["start", "purchase"]
